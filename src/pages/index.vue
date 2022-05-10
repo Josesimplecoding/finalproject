9f65d1f953dc2bdb79ded4242bf49f27f07a25c7
Josesimplecoding <Jose.salazar9877@gmail.com>
2022-05-09 19:41:29 -0500

    moved buttons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,36 +9,36 @@ const { isAuthenticated, user } = useAuth();
     <h2 v-else class="text-6xl tracking-tighter font-extralight">Welcome {{user}}</h2>
     <img src="../assets/bgindex.png" alt="Hello BG"/>
 </div>
-    <h1>Welcome to the the most affordable insurance for not only the first time drivers.</h1>
+    <h1>Welcome to the the most affordable insurance for not only the first time drivers but for all those who happen to want an affordable insurance for their daily driver, the cruiser, special occasions, or for their collectibles. We offer insurance for all types of vehiles, if they hold a value to you then why not protect them with something you can afford.</h1>
     <div class="flex items-center justify-center rounded-lg shadow-2xl">
         <form class="flex-col p-2 space-y-2"/>
     </div>
 
-        <button type="submit" class="w-1/3 py-2 text-indigo-100 bg-gray-800 hover:bg-gray-600">
+        <button type="submit" class="w-1/3 py-2 text-indigo-100 bg-blue-700 rounded-lg hover:bg-blue-500">
             <div class="navigation-items__item navigation-items__item--level-2">
                 <a class="navigation-items__link" href="/about">
-                    About Us,
+                    About Us
                     <h2>Learn a little more about us and see why we chose the RGV as our start to helping every driver that needs affordable insurance</h2>
                 </a>
             </div>
         </button>
-        
 
-        <div class="navigation-items__item navigation-items__item--level-2">
-            <button type="submit" class="w-1/3 py-2 text-indigo-100 bg-gray-800 hover:bg-gray-600">
+
+        <button type="submit" class="w-1/3 py-2 text-indigo-100 bg-blue-700 rounded-lg hover:bg-blue-500">
+            <div class="navigation-items__item navigation-items__item--level-2">
                 <a class="navigation-items__link" href="/available">
                     Available Insurance,
                     <h3>want to find out if we cover you necessary needs? click here to see our list of available vehicle coverage</h3>
                 </a>
-            </button>
-        </div>
+            </div>
+        </button>
 
-        <div class="navigation-items__item navigation-items__item--level-2">
-            <button type="submit" class="w-1/3 py-2 text-indigo-100 bg-gray-800 hover:bg-gray-600">
-                <a class="navigation-items__link" href="/quote">
+        <button type="submit" class="w-1/3 py-2 text-indigo-100 bg-blue-700 rounded-lg hover:bg-blue-500">
+            <div class="navigation-items__item navigation-items__item--level-2">
+                <a class="navigation-items__link" href="/available">
                     Get a Quote
                     <h4>want to find out if we're the right choice for you and your vehicle? Click here to get a quote from us with just a few questions</h4>
                 </a>
-            </button>
-        </div>
+            </div>
+        </button>
 </template>
