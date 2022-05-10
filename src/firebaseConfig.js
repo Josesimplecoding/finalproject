076c36c05a9765365a9c4ddb18bb938524ebcb65
Josesimplecoding <Jose.salazar9877@gmail.com>
2022-05-09 20:47:41 -0500

    removed firebase

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCj2ZstjEIgfCcx2ABfgEdw6lmgMzaimVU",
-    authDomain: "vue-3-login-test-85227.firebaseapp.com",
-    projectId: "vue-3-login-test-85227",
-    storageBucket: "vue-3-login-test-85227.appspot.com",
-    messagingSenderId: "558407049398",
-    appId: "1:558407049398:web:4081957080dd701d494617"
-};
